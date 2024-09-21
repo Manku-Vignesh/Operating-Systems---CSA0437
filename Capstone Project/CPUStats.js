febffc76fd24191ba22abd0dfb7466e0b5eae918
rk_vignesh <119102507+Manku-Vignesh@users.noreply.github.com>
2024-09-21 12:39:45 +0530

    Create CPUStats.js

---
+++ Capstone Project/CPUStats.js
@@ -0,0 +1,9 @@
+class CPUStats {
+    constructor(model, speed, usage) {
+        this.model = model;
+        this.speed = speed;
+        this.usage = usage;
+    }
+}
+
+module.exports = CPUStats;
